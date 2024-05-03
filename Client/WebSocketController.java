f388b770f584ad58db9af09fbb625c620db31103
mattjoelar <maol7318@colorado.edu>
2024-05-03 03:00:08 -0600

    Moved all Client related files into the Client Directory

---
+++ Client/WebSocketController.java
@@ -0,0 +1,27 @@
+package com.ooadproject.wstutorial;
+
+
+import com.ooadproject.wstutorial.dto.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebSocketController {
+
+    @Autowired
+    private WebSocketService service;
+
+    @PostMapping("/send-message")
+    public void sendMessage(@RequestBody final Message message) {
+        service.notifyFrontend(message.getMessageContent());
+    }
+
+    @PostMapping("/send-private-message/{id}")
+    public void sendPrivateMessage(@PathVariable final String id,
+                                   @RequestBody final Message message) {
+        service.notifyUser(id, message.getMessageContent());
+    }
+}
